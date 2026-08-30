1e180ade625304de8f4d6d70eb695976ca24c580
Navis Ayara <navisayara@gmail.com>
2026-08-30 12:09:39 +0300

    style: adds a fade animation for the compliments that shows between generations

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,12 @@
 
     let compliment = $state("");
     let selectedCategory = $state(categories[0]); // defaults to the first category of compliments
+    let isExiting = $state(false);
 
     function generateCompliment() {
       // picks a random compliment from the `complimentList` store
+
+      isExiting = true
       if (selectedCategory !== "random") {
         const index = Math.floor(Math.random() * complimentsList[selectedCategory].length);
         compliment = complimentsList[selectedCategory][index];
@@ -22,7 +25,9 @@
 </script>
 
 <main>
-    <p>{compliment}</p>
+    <p class="{isExiting ? 'slide' : ''}" onanimationend={() => {
+      isExiting = false
+    }}>{compliment}</p>
     <div class="controls-container">
         <button onclick={generateCompliment}>Get gassed</button>
         <ul>
@@ -78,6 +83,21 @@
         width: fit-content;
         margin: auto;
         font-size: 2rem;
+        transition: transform 0.2s ease;
+    }
+
+    p.slide {
+        animation: fade-out-in 0.3s ease-in-out;
+    }
+
+    @keyframes fade-out-in {
+        2% {
+            opacity: 2%;
+        }
+
+        100% {
+            opacity: 100%;
+        }
     }
 
     .controls-container > button {
